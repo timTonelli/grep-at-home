88bb754d4fd457caf2308f4a90ce2d201725dfcb
timTonelli <102175985+timTonelli@users.noreply.github.com>
2023-05-29 16:15:37 -0400

    dry up main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use clap::Parser;
 use std::{
     fs::File,
-    io::{stdin, BufRead, BufReader},
+    io::{stdin, BufRead, BufReader, Read},
+    path::PathBuf,
 };
 
 /// Search for a pattern in a filepath, and display lines that contain it
@@ -31,14 +32,12 @@ fn match_from_reader<T: BufRead>(needle: &str, haystack: T) -> Vec<String> {
 */
 fn main() -> Result<(), GahError> {
     let args = Cli::parse();
-    let matches;
-    if args.filepath == std::path::PathBuf::from("-") {
-        let stdin_reader = BufReader::new(stdin().lock());
-        matches = match_from_reader(&args.pattern, stdin_reader);
+    let input: Box<dyn Read> = if args.filepath == PathBuf::from("-") {
+        Box::new(stdin().lock())
     } else {
-        let file_reader = BufReader::new(File::open(&args.filepath).unwrap());
-        matches = match_from_reader(&args.pattern, file_reader);
-    }
+        Box::new(File::open(&args.filepath).unwrap())
+    };
+    let matches = match_from_reader(&args.pattern, BufReader::new(input));
 
     println!("{}", matches.join("\n"));
     Ok(())
