7be4a379e24659076e2be2e1fe5fc956217b0212
timTonelli <102175985+timTonelli@users.noreply.github.com>
2023-05-29 09:40:47 -0400

    add initial implementation with clap

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
-fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given!");
-    let path = std::env::args().nth(2).expect("no path given!");
+use clap::Parser;
 
-    println!("Searching for line containing {} in {}", pattern, path);
+/// Search for a pattern in a filepath, and display lines that contain it
+#[derive(Parser)]
+struct Cli {
+    /// Pattern to look for
+    #[arg(long)]
+    pattern: String,
+
+    /// Path of where to search for the pattern
+    #[arg(long)]
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
